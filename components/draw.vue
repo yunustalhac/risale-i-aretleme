67860375da78ecae4678a81089662b67c8c9e207
yunustalhac <yunustalhacoban@gmail.com>
2024-10-03 22:45:44 +0300

    çizim mantığı eklendi

--- components/draw.vue
+++ components/draw.vue
@@ -31,12 +31,11 @@ const eser = ref("lemalar")
 const defaultSayfa = `https://oku.risale.online/images/risale/mektubat/001.png`
 
 const lines = ref([]) // Çizilen çizgilerin tutulduğu array
-const isClick = ref(false)
 const cizgiGenisligi = ref(25)
 
 let x1 = null
 let y1 = null
-const tempLine = ref(null) // Geçici çizgi için
+const isDrawing = ref(false) // Çizim durumu
 
 // Sayfa değiştiğinde çizgileri sıfırla
 const git = (sayfa) => {
@@ -49,136 +48,114 @@ const git = (sayfa) => {
   }
   // Çizgileri sıfırla
   lines.value = []
-  tempLine.value = null
 }
 
-const click = (X, event) => {
-  const startLine1X = X.pageX
-  const startLine1Y = X.pageY
+// Çizim başlat
+const startDrawing = (event) => {
+  x1 = event.pageX
+  y1 = event.pageY
+  isDrawing.value = true
+}
 
-  if (!isClick.value) {
-    // İlk tıklamada başlangıç noktası belirleniyor
-    x1 = startLine1X
-    y1 = startLine1Y
-    isClick.value = true
-  } else {
-    // İkinci tıklamada çizgiyi kalıcı hale getir
-    const x2 = startLine1X
-    const y2 = startLine1Y
+// Çizim yap
+const draw = (event) => {
+  if (!isDrawing.value) return
+  const x2 = event.pageX
+  const y2 = event.pageY
 
-    lines.value.push({
-      x1: x1,
-      y1: y1,
-      x2: x2,
-      y2: y2,
-      color: secilenRenk.value || '#c42f1f', // Rengi burada ekle
-      cizgiGenisligi: cizgiGenisligi.value || "25px",
-    })
-    // Geçici çizgiyi temizle
-    tempLine.value = null
-    isClick.value = false
-  }
+  // Yeni bir çizgi ekle
+  lines.value.push({
+    x1: x1,
+    y1: y1,
+    x2: x2,
+    y2: y2,
+    color: secilenRenk.value || '#c42f1f', // Rengi burada ekle
+    cizgiGenisligi: cizgiGenisligi.value || "25px",
+  })
+
+  // Başlangıç noktalarını güncelle
+  x1 = x2
+  y1 = y2
+}
+
+// Çizimi durdur
+const stopDrawing = () => {
+  isDrawing.value = false
 }
 
 // ESC tuşuna basıldığında çizgi işlemini iptal et
 const handleKeydown = (event) => {
-  if (event.key === 'Escape' && isClick.value) {
-    // Çizgi çekme işlemini iptal et
-    tempLine.value = null
-    isClick.value = false
+  if (event.key === 'Escape' && isDrawing.value) {
+    stopDrawing()
   }
 }
 
-// Component yüklendiğinde keydown olayını dinlemeye başla
+// Component yüklendiğinde olay dinleyicilerini ekle
 onMounted(() => {
   window.addEventListener('keydown', handleKeydown)
 })
 
-// Component destroyed olduğunda keydown olayını kaldır
+// Component destroyed olduğunda olay dinleyicilerini kaldır
 onUnmounted(() => {
   window.removeEventListener('keydown', handleKeydown)
 })
 
-// Fare hareket ettikçe geçici çizgiyi güncelle
-const mouse = (X) => {
-  if (isClick.value) {
-    const x2 = X.pageX
-    const y2 = X.pageY
-    // Geçici çizgiyi güncelle
-    tempLine.value = {
-      x1: x1,
-      y1: y1,
-      x2: x2,
-      y2: y2,
-    }
-  }
-}
 </script>
 
 <template>
-  <div>
-    <div class="absolute right-3 flex flex-col gap-3 top-10">
-      <div class="flex flex-col">
-        <label>eser seçiniz</label>
-        <select v-model="eser" class="border rounded-lg">
-          <option disabled value="">Eser seçiniz</option> <!-- İlk seçeneği disabled yap ve varsayılan değer ver -->
-          <option v-for="eser in eserler" :key="eser" :value="eser">{{ eser }}</option>
-        </select>
-      </div>
-      <div class="flex flex-col">
-        <label>sayfa seçiniz </label>
-        <input class="border rounded-lg" type="text" placeholder="sayfa" v-model="sayfaSayi"
-               @keydown.enter="git(sayfaSayi)">
-      </div>
-      <button class=" border rounded-lg p-2 bg-slate-300 hover:bg-slate-400 hover:text-white" @click="git(sayfaSayi)">sayfaya git</button>
-      <div class="flex flex-col">
-        <span>renk seçiniz</span>
-        <div class="flex gap-3">
-          <select v-model="secilenRenk" class="border rounded-lg">
-            <option disabled value="">renkler</option>
-            <option selected value="#c42f1f">bordo</option>
-            <option v-for="renk in renkler" :key="renk" :value="renk" class="text-black p-2"
-                    :style="{ background: renk }">{{ renk }}
-            </option>
-          </select>
-          <span :style="{ background: secilenRenk }" class="p-4 rounded-full"></span>
-        </div>
-      </div>
-      <div class="flex flex-col">
-        çizgi kalınlığı
-        <input type="range" v-model="cizgiGenisligi" min="15" max="50" step="1">
-        {{ cizgiGenisligi }}
-      </div>
-    </div>
-
-    <div @mousemove="mouse">
-      <div class="grid">
-        <div @click="click" class="">
-          <div class="h-full w-[85%] relative">
+  <div class="flex flex-col lg:flex-row ">
+    <div @mousedown="startDrawing" @mouseup="stopDrawing" @mousemove="draw" class="relative">
+      <div class="">
+        <div class="">
+          <div class="h-full w-full lg:w-[85%] relative">
             <div class="w-full h-full absolute">
               <svg class="w-full h-full">
                 <!-- Çizilen tüm çizgileri döngüyle render et -->
                 <line v-for="(line, index) in lines" :key="index"
                       :x1="line.x1" :y1="line.y1" :x2="line.x2" :y2="line.y2"
                       :stroke="line.color" :stroke-width="line.cizgiGenisligi" stroke-linecap="round"
-                      style="filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.3))"/>
-                <!-- Geçici çizgiyi çiz -->
-                <line v-if="tempLine"
-                      :x1="tempLine.x1" :y1="tempLine.y1" :x2="tempLine.x2" :y2="tempLine.y2"
-                      :stroke="secilenRenk||'#c42f1f'" :stroke-width="cizgiGenisligi" stroke-linecap="round"
-                      stroke-dasharray="5, 5" style="filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.3))"/>
+                      />
               </svg>
             </div>
-            <img draggable="false" class="h-full relative cursor-text border-2 border-black rounded-lg" v-for="item in 1" :key="item" :src="sayfaAc || defaultSayfa">
+            <img draggable="false" class="lg:h-full relative cursor-text border-2 border-black rounded-lg object-contain" v-for="item in 1" :key="item" :src="sayfaAc || defaultSayfa">
           </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="w-1/4 mt-5">
+      <div class="flex flex-col gap-3 w-fit">
+        <div class="flex flex-col">
+          <label>eser seçiniz</label>
+          <select v-model="eser" class="border rounded-lg">
+            <option disabled value="">Eser seçiniz</option>
+            <option v-for="eser in eserler" :key="eser" :value="eser">{{ eser }}</option>
+          </select>
+        </div>
+        <div class="flex flex-col">
+          <label>sayfa seçiniz </label>
+          <input class="border rounded-lg" type="text" placeholder="sayfa" v-model="sayfaSayi"
+                 @keydown.enter="git(sayfaSayi)">
+        </div>
+        <button class="border rounded-lg p-2 bg-slate-300 hover:bg-slate-400 hover:text-white" @click="git(sayfaSayi)">sayfaya git</button>
+        <div class="flex flex-col">
+          <span>renk seçiniz</span>
+          <div class="flex gap-3">
+            <select v-model="secilenRenk" class="border rounded-lg">
+              <option disabled value="">renkler</option>
+              <option selected value="#c42f1f">bordo</option>
+              <option v-for="renk in renkler" :key="renk" :value="renk" class="text-black p-2"
+                      :style="{ background: renk }">{{ renk }}
+              </option>
+            </select>
+            <span :style="{ background: secilenRenk }" class="p-4 rounded-full"></span>
+          </div>
+        </div>
+        <div class="flex flex-col">
+          çizgi kalınlığı
+          <input type="range" min="1" max="50" v-model="cizgiGenisligi">
         </div>
       </div>
     </div>
   </div>
 </template>
-
-<style>
-* {
-  user-select: none;
-}
-</style>
