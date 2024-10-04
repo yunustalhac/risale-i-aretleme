e9555cfdfaab858b44d5051f1e62943c44d7ce34
yunustalhac <yunustalhacoban@gmail.com>
2024-10-04 10:46:45 +0300

    tasarım değişti localStorage eklendi büyük güncelleme

--- components/draw.vue
+++ components/draw.vue
@@ -1,193 +1,209 @@
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue'
+import {ref, onMounted} from 'vue'
 
-const sayfaSayi = ref(0)
-const sayfaAc = ref(0)
-const secilenRenk = ref("")
+const route = useRoute()
+const router = useRouter()
+
 const eserler = [
-  "lemalar",
-  "sozler",
-  "sualar",
-  "mektubat",
-  "siracun-nur",
-  "zulfikar",
-  "asa-yi-musa",
-  "tilsimlar",
-  "isaratul-i-caz",
-  "mesnevi-i-nuriye",
-  "sikke-i-tasdik-i-gaybi",
-  "barla-lahikasi",
-  "kastamonu-lahikasi",
-  "emirdag-lahikasi-1",
-  "emirdag-lahikasi-2",
-  "emirdag-lahikasi-3",
-  "emirdag-lahikasi-4",
-  "bes-risale",
-  "hanimlar-rehberi",
-  "genclik-rehberi",
+  "lemalar", "sozler", "sualar", "mektubat", "siracun-nur", "zulfikar",
+  "asa-yi-musa", "tilsimlar", "isaratul-i-caz", "mesnevi-i-nuriye",
+  "sikke-i-tasdik-i-gaybi", "barla-lahikasi", "kastamonu-lahikasi",
+  "emirdag-lahikasi-1", "emirdag-lahikasi-2", "emirdag-lahikasi-3",
+  "emirdag-lahikasi-4", "bes-risale", "hanimlar-rehberi", "genclik-rehberi"
 ]
+
+const sideBar = ref(false)
+
+const eser = ref(route.query.eser)
+const sayfaSayi = ref(route.query.sayfa)
+const sayfaAc = ref(0)
+
 const renkler = ["red", "yellow", "blue", "purple", "green", "lime", "orange", "gray", "cyan", "navy", "pink", "indigo", "maroon", "magenta", "teal", "brown", "silver", "gold"]
-const eser = ref("lemalar")
-const defaultSayfa = `https://oku.risale.online/images/risale/mektubat/001.png`
+const secilenRenk = ref("")
 
-const lines = ref([]) // Çizilen çizgilerin tutulduğu array
+const lines = ref([])
 const cizgiGenisligi = ref(25)
-
 const isDraw = ref(true)
+const isDrawing = ref(false)
 let x1 = null
 let y1 = null
-const isDrawing = ref(false) // Çizim durumu
 
-// Sayfa değiştiğinde çizgileri sıfırla
-const git = (sayfa) => {
+// LocalStorage anahtarı oluşturma (eser ve sayfa bazlı)
+const localStorageKey = () => `${eser.value}-${sayfaSayi.value}-lines`
+
+const git = async (sayfa) => {
+  await router.push({query: {eser: eser.value, sayfa: sayfaSayi.value}});
+
   if (sayfaSayi.value.length < 2) {
-    sayfaAc.value = `https://oku.risale.online/images/risale/${eser.value}/00${sayfa}.png`
+    sayfaAc.value = `https://oku.risale.online/images/risale/${route.query.eser}/00${route.query.sayfa}.png`
   } else if (sayfaSayi.value.length < 3) {
-    sayfaAc.value = `https://oku.risale.online/images/risale/${eser.value}/0${sayfa}.png`
+    sayfaAc.value = `https://oku.risale.online/images/risale/${route.query.eser}/0${route.query.sayfa}.png`
   } else {
-    sayfaAc.value = `https://oku.risale.online/images/risale/${eser.value}/${sayfa}.png`
+    sayfaAc.value = `https://oku.risale.online/images/risale/${route.query.eser}/${route.query.sayfa}.png`
   }
-  // Çizgileri sıfırla
-  lines.value = []
+
+  const storedLines = localStorage.getItem(localStorageKey())
+  lines.value = storedLines ? JSON.parse(storedLines) : []
 }
 
-// Çizim başlat
 const startDrawing = (event) => {
-  if (!isDraw.value) return; // Çizim kapalıysa çık
-  event.preventDefault(); // Varsayılan davranışı engelle
-  const touch = event.touches ? event.touches[0] : event; // Mobil veya masaüstü durumu
+  if (!isDraw.value) return;
+  event.preventDefault();
+  const touch = event.touches ? event.touches[0] : event;
   x1 = touch.pageX;
   y1 = touch.pageY;
   isDrawing.value = true;
 }
 
-// Çizim yap
 const draw = (event) => {
-  if (!isDrawing.value) return; // Çizim yapma izni yoksa çık
-  event.preventDefault(); // Varsayılan davranışı engelle
-  const touch = event.touches ? event.touches[0] : event; // Mobil veya masaüstü durumu
+  if (!isDrawing.value) return;
+  event.preventDefault();
+  const touch = event.touches ? event.touches[0] : event;
   const x2 = touch.pageX;
   const y2 = touch.pageY;
 
-  // Yeni bir çizgi ekle
   lines.value.push({
     x1: x1,
     y1: y1,
     x2: x2,
     y2: y2,
-    color: secilenRenk.value || "gold", // Rengi burada ekle
+    color: secilenRenk.value || "gold",
     cizgiGenisligi: cizgiGenisligi.value || "25px",
-  })
+    eserim: route.query.eser,
+    sayfam: route.query.sayfa,
+  });
 
-  // Başlangıç noktalarını güncelle
+  // Çizimleri localStorage'a kaydet
+  localStorage.setItem(localStorageKey(), JSON.stringify(lines.value));
+
   x1 = x2;
   y1 = y2;
 }
 
-// Çizimi durdur
 const stopDrawing = () => {
   isDrawing.value = false;
 }
 
-// ESC tuşuna basıldığında çizgi işlemini iptal et
-const handleKeydown = (event) => {
-  if (event.key === 'Escape' && isDrawing.value) {
-    stopDrawing();
-  }
-}
-
 const temizle = () => {
-  lines.value = []
-  
+  lines.value = [];
+  localStorage.removeItem(localStorageKey());
 }
 
-
-// Component yüklendiğinde olay dinleyicilerini ekle
 onMounted(() => {
-  window.addEventListener('keydown', handleKeydown);
-})
-
-// Component destroyed olduğunda olay dinleyicilerini kaldır
-onUnmounted(() => {
-  window.removeEventListener('keydown', handleKeydown);
-})
-
+  git()
+});
 </script>
 
 <template>
-  <div class="flex flex-col lg:flex-row ">
+  <div class="flex flex-col">
     <div @touchstart="startDrawing" @touchend="stopDrawing" @touchmove="draw"
          @mousedown="startDrawing" @mouseup="stopDrawing" @mousemove="draw" class="relative">
       <div class="">
-        <div class="">
-          <button
-              class="fixed top-0 left-0 bg-blue-950 text-white p-2 z-50 rounded-lg"
-              @touchstart.prevent="isDraw = !isDraw"
-              @mousedown.prevent="isDraw = !isDraw"
-          >
-            <span v-if="isDraw">çizimi kapat</span>
-            <span v-else>çizimi aç</span>
-          </button>
+        <div>
 
 
-          <div class="h-full w-full lg:w-[85%] relative">
+          <div class="h-full w-full relative">
             <div class="w-full h-full absolute">
               <svg class="w-full h-full">
-                <!-- Çizilen tüm çizgileri döngüyle render et -->
                 <line v-for="(line, index) in lines" :key="index"
                       :x1="line.x1" :y1="line.y1" :x2="line.x2" :y2="line.y2"
-                      :stroke="line.color" :stroke-width="line.cizgiGenisligi" stroke-linecap="round"
-                />
+                      :stroke="line.color" :stroke-width="line.cizgiGenisligi" stroke-linecap="round"/>
               </svg>
             </div>
-            <img draggable="false" class="lg:h-full relative cursor-text border-2 border-black rounded-lg object-contain" v-for="item in 1" :key="item" :src="sayfaAc || defaultSayfa">
+            <img draggable="false"
+                 class="lg:h-full relative cursor-text border-2 border-black rounded-lg object-contain"
+                 :src="sayfaAc">
           </div>
         </div>
       </div>
     </div>
-
-    <div class="w-1/4 my-5">
-      <div class="flex flex-col gap-3 w-fit">
-        <div class="flex flex-col">
-          <label>eser seçiniz</label>
-          <select v-model="eser" class="border rounded-lg">
-            <option disabled value="">Eser seçiniz</option>
-            <option v-for="eser in eserler" :key="eser" :value="eser">{{ eser }}</option>
-          </select>
-        </div>
-        <div class="flex flex-col">
-          <label>sayfa seçiniz </label>
-          <input class="border rounded-lg" type="text" placeholder="sayfa" v-model="sayfaSayi"
-                 @keydown.enter="git(sayfaSayi)">
-        </div>
-        <button class="border rounded-lg p-2 bg-slate-300 hover:bg-slate-400 hover:text-white" @click="git(sayfaSayi)">sayfaya git</button>
-        <div class="flex flex-col">
-          <span>renk seçiniz</span>
-          <div class="flex gap-3">
-            <select v-model="secilenRenk" class="border rounded-lg">
-              <option disabled value="">renkler</option>
-              <option v-for="renk in renkler" :key="renk" :value="renk" class="text-black p-2"
-                      :style="{ background: renk }">{{ renk }}
-              </option>
+    <div class="fixed top-0 w-full sideAnimation">
+      <div v-if="sideBar" class="p-4 bg-white shadow-lg rounded-lg transition-all sideAnimation  mx-auto">
+        <div class="flex justify-around items-center gap-4">
+          <div class="flex flex-col">
+            <label class="font-semibold">Eser Seçiniz</label>
+            <select v-model="eser" class="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500">
+              <option disabled value="">Eser seçiniz</option>
+              <option v-for="eser in eserler" :key="eser" :value="eser">{{ eser }}</option>
             </select>
-            <span :style="{ background: secilenRenk }" class="p-4 rounded-full"></span>
+          </div>
+
+          <div class="flex flex-col">
+            <label class="font-semibold">Sayfa Seçiniz</label>
+            <input class="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500" type="text" placeholder="sayfa" v-model="sayfaSayi" @keydown.enter="git(sayfaSayi)">
+          </div>
+
+          <button class="border rounded-lg p-2 bg-blue-600 text-white hover:bg-blue-700 transition duration-300" @click="git(sayfaSayi)">Sayfaya Git</button>
+
+          <div class="flex flex-col">
+            <span class="font-semibold">Renk Seçiniz</span>
+            <div class="flex gap-2">
+              <select v-model="secilenRenk" class="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                <option disabled value="">Renkler</option>
+                <option v-for="renk in renkler" :key="renk" :value="renk" class="text-black p-2" :style="{ background: renk }">{{ renk }}</option>
+              </select>
+              <span :style="{ background: secilenRenk }" class="p-4 rounded-full border"></span>
+            </div>
+          </div>
+
+          <div class="flex flex-col">
+            <span class="font-semibold">Çizgi Kalınlığı</span>
+            <input type="range" min="1" max="50" v-model="cizgiGenisligi" class="slider">
+            <span>{{ cizgiGenisligi }}</span>
+          </div>
+
+          <div>
+            <button @click="temizle" class="bg-red-600 text-white p-2 rounded-lg hover:bg-red-700 transition duration-300">Çizimleri Temizle</button>
+          </div>
+
+          <div>
+            <button class="cursor-pointer bg-green-600 text-white p-2 rounded-lg hover:bg-green-700 transition duration-300" @touchstart.prevent="isDraw = !isDraw" @mousedown.prevent="isDraw = !isDraw">
+              <span v-if="isDraw">Çizimi Kapat</span>
+              <span v-else>Çizimi Aç</span>
+            </button>
           </div>
         </div>
-        <div class="flex flex-col">
-          çizgi kalınlığı
-          <input type="range" min="1" max="50" v-model="cizgiGenisligi">
-          {{cizgiGenisligi}}
-        </div>
-        <div>
-          <button @click="temizle" class="bg-red-800 text-white p-2 rounded-lg">çizimleri temizle</button>
-        </div>
+      </div>
+      <div class="flex justify-center ">
+        <button @click="sideBar = !sideBar" class="p-3 bg-white shadow-black shadow-sm">kontrol paneli</button>
       </div>
     </div>
   </div>
 </template>
 
 <style>
-* {
-  user-select: none;
+@keyframes animate {
+  0% {
+    opacity: 0;
+    transform: translateY(-100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
 }
+
+.sideAnimation {
+  animation: animate 0.5s ease-in-out;
+}
+
+.slider {
+  -webkit-appearance: none;
+  width: 100%;
+  height: 6px;
+  border-radius: 5px;
+  background: #ddd;
+  outline: none;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background: #4f46e5; /* Modern bir mavi tonu */
+  cursor: pointer;
+}
+
 </style>
